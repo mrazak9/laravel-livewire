98ce816ba46a3526ce26f7cfba3d53ea6561799c
mrazak9 <muhamadrozak9@gmail.com>
2024-02-02 16:19:14 +0700

    fungsi edit

--- app/Livewire/Employee.php
+++ app/Livewire/Employee.php
@@ -11,9 +11,11 @@ class Employee extends Component
     use WithPagination;
     protected $paginationTheme = 'bootstrap';
 
+    public $employee_id;
     public $nama;
     public $email;
     public $alamat;
+    public $updateData = false;
 
     public function store()
     {
@@ -29,9 +31,49 @@ class Employee extends Component
             'alamat.required' => 'Alamat Wajib diisi',
         ];
         $validated = $this->validate($rules, $pesan);
-
         ModelsEmployee::create($validated);
+        $this->clear();
         session()->flash('message', 'Data berhasil disimpan');
+    }
+
+    public function edit($id)
+    {
+        $data = ModelsEmployee::find($id);
+        $this->employee_id = $id;
+        $this->nama = $data->nama;
+        $this->email = $data->email;
+        $this->alamat = $data->alamat;
+        $this->updateData = true;
+    }
+
+
+    public function update()
+    {
+        $rules = [
+            'nama' => 'required',
+            'email' => 'required|email',
+            'alamat' => 'required',
+        ];
+        $pesan = [
+            'nama.required' => 'Nama Wajib diisi',
+            'email.required' => 'Email Wajib diisi',
+            'email.email' => 'Format Email tidak sesuai',
+            'alamat.required' => 'Alamat Wajib diisi',
+        ];
+        $validated = $this->validate($rules, $pesan);
+        $data = ModelsEmployee::find($this->employee_id);
+        $data->update($validated);
+        $this->clear();
+        session()->flash('message', 'Data berhasil diubah');
+    }
+
+    public function clear()
+    {
+        $this->employee_id = '';
+        $this->nama = '';
+        $this->email = '';
+        $this->alamat = '';
+        $this->updateData = false;
     }
 
     public function render()
--- resources/views/livewire/employee.blade.php
+++ resources/views/livewire/employee.blade.php
@@ -41,8 +41,14 @@
                 </div>
                 <div class="mb-3 row">
                     <label class="col-sm-2 col-form-label"></label>
-                    <div class="col-sm-10"><button type="button" class="btn btn-primary" name="submit"
-                            wire:click="store()">SIMPAN</button>
+                    <div class="col-sm-10">
+                        @if ($updateData == false)
+                            <button type="button" class="btn btn-primary" name="submit"
+                                wire:click="store()">SIMPAN</button>
+                        @else
+                            <button type="button" class="btn btn-primary" name="submit"
+                                wire:click="update()">Update</button>
+                        @endif
                     </div>
                 </div>
             </form>
@@ -70,8 +76,8 @@
                             <td>{{ $value->email }}</td>
                             <td>{{ $value->alamat }}</td>
                             <td>
-                                <a href="" class="btn btn-warning btn-sm">Edit</a>
-                                <a href="" class="btn btn-danger btn-sm">Del</a>
+                                <a wire:click="edit({{ $value->id }})" class="btn btn-warning btn-sm">Edit</a>
+                                <a wire:click="" class="btn btn-danger btn-sm">Del</a>
                             </td>
                         </tr>
                     @endforeach
